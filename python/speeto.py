17041fbc497f153cf9827a5809fc042c77c806fe
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-07-27 02:55:25 +0700

    fixing

--- python/speeto.py
+++ python/speeto.py
@@ -26,8 +26,8 @@ class speeto:
 
     def create_socket(self, PORT = 8989):
         HOST = ''
-
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+
             sock.bind((HOST, PORT))
             sock.listen(1)
             conn, addr = sock.accept()
@@ -36,27 +36,64 @@ class speeto:
             with conn:
                 print('connected to ', addr)
                 while True:
-                    data = conn.recv(1024)
-                    if not data: break
-                    conn.sendall(data)
+                    handshake = conn.recv(1024)
+                    if not handshake: break;
+                    if handshake.decode('utf-8') =="test_speed" :
+
+                        conn.sendall(b'accept1')
+                        data = conn.recv(1024)
+                        print('data is ',data)
+                        data = conn.recv(1024)
+                        print('data is ', data)
+
+                    sock.close()
 
     def connect_socket(self, HOST , PORT):
         # handshake
+        QUOTA_1 = 1000
+        QUOTA_2 = 100000
+        QUOTA_3 = 5000000
+
+
         base_time = time.time()
         print('Connecting.... to ',HOST, ' with port ',PORT)
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 sock.connect((HOST, PORT))
-                sock.sendall(b'helllo')
-                data = sock.recv(1024)
+                sock.sendall(b'test_speed')
+                acc = sock.recv(1024)
+                print('acc is ',acc)
+                if acc.decode('utf-8') == "accept1":
+                    self.latency = time.time() - base_time
+                    # test uplink speed
+                    print("connected, testing speed...")
+                    # speed test 1
+                    base_time_speed1 = time.time()
+                    sock.sendall(b'.' * QUOTA_1)
+                    self.speed1 = time.time() - base_time_speed1
+                    print('speed 1 is ', self.speed1)
+
+                    # speed test 2
+                    base_time_speed2 = time.time()
+                    sock.sendall(b'.' * QUOTA_2)
+                    self.speed2 = time.time() - base_time_speed2
+                    print('speed 2 is ', self.speed2)
+
+                    # speed test 3
+                    base_time_speed3 = time.time()
+                    sock.sendall(b'.' * QUOTA_3)
+                    self.speed3 = time.time() - base_time_speed3
+                    print('speed 3 is ', self.speed3)
+
+                sock.close()
         except ConnectionRefusedError:
             print('Server refusing connection')
             print('maybe the listener not run yet')
 
             return -1
-        self.latency = time.time()-base_time
-        print('latency is ',self.latency)
-        print('get ', data)
+
+        print('latency is ', self.latency)
+
         return True
 
     def test_speed(self, HOST, PORT):

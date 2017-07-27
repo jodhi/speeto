b18c8948d1cac76911190f1d0eb4e0df01ef364d
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-07-27 16:35:41 +0700

    update

--- python/speeto.py
+++ python/speeto.py
@@ -5,11 +5,11 @@ class speeto:
         self.start = 0
         self.temp_file_name = '.speeto_temp'
         self.temp_file_use = False
-        self.QUOTA_1 = 100000
-        self.QUOTA_2 = 500000
-        self.QUOTA_3 = 1000000
-        self.QUOTA_4 = 2000000
-        self.QUOTA_5 = 5000000
+        self.QUOTA_1 = 131072
+        self.QUOTA_2 = 524288
+        self.QUOTA_3 = 1048576
+        self.QUOTA_4 = 2097152
+        self.QUOTA_5 = 4194304
         # load/check saved configuration (last/saved session) from local storage
         try:
             with open(self.temp_file_name,'r') as temp_file:
@@ -49,7 +49,7 @@ class speeto:
                         data = conn.recv(self.QUOTA_5)
                         data = conn.recv(self.QUOTA_5)
                         data = conn.recv(self.QUOTA_5)
-
+                        print(len(data))
                         conn.sendall(b'accept2')
 
                         # speed test 1
@@ -60,12 +60,12 @@ class speeto:
 
                         # speed test 2
                         base_time_speed2 = time.time()
-                        conn.sendall(b'.' * self.QUOTA_4)
-                        self.speed_down2 = (self.QUOTA_4 / 1000) / (time.time() - base_time_speed1)
+                        conn.sendall(b'.',self.QUOTA_3)
+                        self.speed_down2 = (self.QUOTA_3 / 1000) / (time.time() - base_time_speed2)
                         print('speed 2 is ', self.speed_down2, 'KBps')
 
                         conn.sendall(self.speed_down1)
-                        conn.sendall(self.speed_down2)
+                        # conn.sendall(self.speed_down2)
                     sock.close()
 
     def connect_socket(self, HOST , PORT):
@@ -103,13 +103,17 @@ class speeto:
                     print('speed 3 is ', self.speed3 , 'KBps')
 
                     # condition for more test if too fast
-
-                if acc.decode('utf-8') == "accept1":
+                
+                acc = sock.recv(1024)
+                if acc.decode('utf-8') == "accept2":
                     print("Testing Download Speed")
                     # Download Speed
-                    data = sock.recv(102400)
-                    data = sock.recv(102400)
-
+                    data = sock.recv(10002400)
+                    print(len(data))
+                    data = sock.recv(10002400)
+                    print(len(data))
+                    print(sock.recv(2048))
+                    print(sock.recv(2048))
                 sock.close()
         except ConnectionRefusedError:
             print('Server refusing connection')
